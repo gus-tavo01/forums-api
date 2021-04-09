c8b2a7cfd568026b7f3581151a3105c99512db5c
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-08 18:50:11 -0700

    create topic model schema

---
+++ models/Topic.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const TopicSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createDate: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  updatedDate: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  comments: {
+    type: [
+      {
+        id: {
+          type: Schema.Types.ObjectId,
+          required: true,
+        },
+        from: { type: String, required: true },
+        to: { type: String, required: false, default: null },
+        message: { type: String, required: true },
+        createDate: {
+          type: Date,
+          required: false,
+          default: Date.now,
+        },
+        likes: { type: Number, default: 0 },
+        dislikes: { type: Number, default: 0 },
+      },
+    ],
+    default: [],
+  },
+});
+
+const Topic = model('Topic', TopicSchema);
+
+module.exports = Topic;
