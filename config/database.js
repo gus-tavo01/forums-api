02055c278bd7e490a9e616b4414fe49356634dfd
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-10 14:23:15 -0700

    fix db configuration

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  const host = process.env.DB_HOST || '127.0.0.1';
-  const dbName = process.env.DB_NAME || 'Forums-Db';
-  const uri = `mongodb://${host}/${dbName}`;
+  const uri = process.env.DB_CONNECTION;
 
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   return mongoose.connection;
