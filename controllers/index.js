b4373f72fb2666269b97bc33fcc2b9c2db4685f2
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-13 21:57:26 -0700

    add topics controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const AuthController = require('./Auth.Controller');
 const ForumsController = require('./Forums.Controller');
 const UsersController = require('./Users.Controller');
+const TopicsController = require('./Topics.Controller');
 
 const router = Router();
 const api = 'api/v0';
@@ -9,10 +10,12 @@ const api = 'api/v0';
 const authController = new AuthController();
 const forumsController = new ForumsController();
 const usersController = new UsersController();
+const topicsController = new TopicsController();
 
 router.use(`/${api}/auth`, authController.router);
 router.use(`/${api}/forums`, forumsController.router);
 router.use(`/${api}/users`, usersController.router);
+router.use(`/${api}/forums/:forumId/topics`, topicsController.router);
 
 // endpoints
 // GET api/v0/forums?...filters
