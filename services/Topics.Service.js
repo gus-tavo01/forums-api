4338d04c4fb6661e73c60c76828b48352b8e96c0
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-13 21:59:25 -0700

    add topics service methods

--- services/Topics.Service.js
+++ services/Topics.Service.js
@@ -10,16 +10,45 @@ class TopicsService {
       // id is valid?
       // validate
       const result = await this.topicsRepository.findById(id);
-      return result;
+      const serviceResponse = { result, fields: [] };
+      return serviceResponse;
     } catch (error) {
       // error actions
     }
   };
 
-  // create
-  // get = async (filters) => {}
+  create = async (topic) => {
+    try {
+      // validate topic data is valid
+      const result = await this.topicsRepository.add(topic);
+      const serviceResponse = { result, fields: [] };
+      return serviceResponse;
+    } catch (error) {
+      // handle service error
+    }
+  };
+
+  get = async (filters) => {
+    try {
+      const result = await this.topicsRepository.find(filters);
+      const serviceResponse = { result, fields: [] };
+      return serviceResponse;
+    } catch (error) {
+      // handle err
+    }
+  };
+
   // update
-  // remove
+
+  remove = async (id) => {
+    try {
+      const result = await this.topicsRepository.remove(id);
+      const serviceResponse = { result, fields: [] };
+      return serviceResponse;
+    } catch (error) {
+      // handle err
+    }
+  };
 }
 
 module.exports = TopicsService;
