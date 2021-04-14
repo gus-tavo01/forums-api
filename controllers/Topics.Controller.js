d16fc7c02ec4a89b17a6c9e27a11c07c3592a6e9
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-13 22:01:28 -0700

    add get topics

--- controllers/Topics.Controller.js
+++ controllers/Topics.Controller.js
@@ -7,8 +7,10 @@ class TopicsController {
     this.router = Router();
     this.topicsService = new TopicsService();
 
-    this.router.get('/:id', this.getById);
+    this.router.get('/', this.get);
     this.router.post('/', this.post);
+    this.router.get('/:id', this.getById);
+    this.router.delete('/:id', this.delete);
   }
 
   getById = async (req, res) => {
@@ -16,6 +18,10 @@ class TopicsController {
     try {
       const { id } = req.params;
       const response = await this.topicsService.getById(id);
+      if (!response.result) {
+        apiResponse.notFound('Topic is not found');
+        return res.status(apiResponse.statusCode).json(apiResponse);
+      }
       apiResponse.ok(response);
     } catch (error) {
       apiResponse.internalServerError(error);
@@ -26,16 +32,42 @@ class TopicsController {
   post = async (req, res) => {
     const apiResponse = new ApiResponse();
     try {
-      // const response = this.topicsService
-      apiResponse.ok();
+      const { body } = req;
+      const response = this.topicsService.create(body);
+      if (!response.result) {
+        apiResponse.unprocessableEntity(
+          'Topic cannot be created, please try again later'
+        );
+        return res.status(apiResponse.statusCode).json(apiResponse);
+      }
+      apiResponse.ok(response);
     } catch (error) {
       apiResponse.internalServerError(error);
     }
     return res.status(apiResponse.statusCode).json(apiResponse);
   };
 
-  // get
-  // delete
+  get = async (req, res) => {
+    const apiResponse = new ApiResponse();
+    try {
+      const filters = req.query;
+      const response = await this.topicsService.get(filters);
+      apiResponse.ok(response);
+    } catch (error) {
+      apiResponse.internalServerError(error);
+    }
+    return res.status(apiResponse.statusCode).json(apiResponse);
+  };
+
+  delete = async (req, res) => {
+    const apiResponse = new ApiResponse();
+    try {
+    } catch (error) {
+      apiResponse.internalServerError(error);
+    }
+    return res.status(apiResponse.statusCode).json(apiResponse);
+  };
+
   // patch
 }
 
