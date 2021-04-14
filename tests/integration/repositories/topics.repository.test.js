0e76c0b2dab7013bbe8e27727cdcb5663fe7e469
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-13 18:46:02 -0700

    add topics repo integration test

---
+++ tests/integration/repositories/topics.repository.test.js
@@ -0,0 +1,21 @@
+const TopicsRepository = require('../../../repositories/Topics.Repository');
+
+// TODO:
+// setup DB
+// beforeAll(() => {});
+
+// destroy connection
+// afterAll(() => {});
+
+describe('Topics Repository find', () => {
+  test('When filters are op, expect to retrieve a paginated result', async () => {
+    // Arrange
+    const filters = {};
+
+    // Act
+
+    // Assert
+  });
+});
+
+// describe('Topics Repository findById', () => {});
