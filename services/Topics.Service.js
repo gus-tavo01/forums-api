aa5f8a6aef261995c9287012e1b50e239932be22
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-14 22:58:06 -0700

    remove second parameter forumId

--- services/Topics.Service.js
+++ services/Topics.Service.js
@@ -31,7 +31,7 @@ class TopicsService {
   get = async (filters) => {
     try {
       // ... filters object is not null
-      const result = await this.topicsRepository.find(filters, forumId);
+      const result = await this.topicsRepository.find(filters);
       const serviceResponse = { result, fields: [] };
       return serviceResponse;
     } catch (error) {
@@ -39,7 +39,7 @@ class TopicsService {
     }
   };
 
-  // update
+  // update = async (patch) => { try{}catch(error){} };
 
   remove = async (id) => {
     try {
