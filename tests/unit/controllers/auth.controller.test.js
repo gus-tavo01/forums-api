8af41ec63e914c2c51dfea3cbb7acac10815ed81
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-17 00:51:01 -0700

    implement getResponse helper

--- tests/unit/controllers/auth.controller.test.js
+++ tests/unit/controllers/auth.controller.test.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
-const { getMockReq, getMockRes } = require('@jest-mock/express');
+const mockResponse = require('../test-helpers/mockRes');
+const { getMockReq } = require('@jest-mock/express');
 const AuthController = require('../../../controllers/Auth.Controller');
 const LoginsService = require('../../../services/Logins.Service');
+const UsersService = require('../../../services/Users.Service');
 
 jest.mock('../../../services/Logins.Service');
 jest.mock('../../../services/Users.Service');
 
-const { res, clearMockRes } = getMockRes();
-let authController;
+const { res, clearMockRes } = mockResponse();
 
-describe('Auth controller Login', () => {
-  beforeAll(() => {
-    authController = new AuthController();
-  });
+describe('Auth controller login', () => {
+  let authController = new AuthController();
 
   afterEach(() => {
     clearMockRes();
@@ -45,7 +44,12 @@ describe('Auth controller Login', () => {
     await authController.login(req, res);
 
     // Assert
-    expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    expect(res.response).toHaveBeenCalledWith(
+      expect.objectContaining({
+        payload: expect.anything(),
+        statusCode: expectedStatusCode,
+      })
+    );
   });
 
   // test('When username does not exist, expect a 401 response', async () => {});
@@ -56,3 +60,56 @@ describe('Auth controller Login', () => {
 
   // test('When an unhandled error, expect a 500 response', async () => {});
 });
+
+describe('Auth controller register', () => {
+  let authController = new AuthController();
+
+  afterEach(() => {
+    clearMockRes();
+    // mocks reset
+  });
+
+  test('When user data is valid, expect a successful response', async () => {
+    // Arrange
+    const username = 'paco.perez01';
+    const email = 'pac.per@gmail.com';
+    const dateOfBirth = Date.now();
+    const password = 'abc123!!';
+    const req = getMockReq({
+      body: { username, email, dateOfBirth, password },
+    });
+
+    // mocks
+    const serviceResponse = { fields: [] };
+    const mockLogin = { ...serviceResponse, result: null };
+    LoginsService.prototype.findByUsername = jest.fn(async () => mockLogin);
+    const mockCreateLogin = {
+      ...serviceResponse,
+      result: {
+        username,
+      },
+    };
+    LoginsService.prototype.create = jest.fn(async () => mockCreateLogin);
+    const mockAddUserAccount = {
+      ...serviceResponse,
+      result: { username, email, dateOfBirth },
+    };
+    UsersService.prototype.add = jest.fn(async () => mockAddUserAccount);
+
+    // Act
+    await authController.register(req, res);
+
+    // Assert
+    expect(res.response).toHaveBeenCalledWith(
+      expect.objectContaining({
+        statusCode: 201,
+        payload: expect.anything(),
+      })
+    );
+  });
+
+  // for each param on body, do a test
+  // 'When username is in use'
+});
+
+// describe('Auth controller resetPassword');
