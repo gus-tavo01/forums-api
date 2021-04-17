255022e4f96b1f9eeb23f93c2d8f55bf0248a94a
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-16 21:05:31 -0700

    add reset password method

--- controllers/Auth.Controller.js
+++ controllers/Auth.Controller.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const jsonwebtoken = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const ApiResponse = require('../common/ApiResponse');
 const LoginsService = require('../services/Logins.Service');
 const UsersService = require('../services/Users.Service');
@@ -14,9 +15,11 @@ class AuthController {
 
     this.router.post('/login', this.login);
     this.router.post('/register', this.register);
-    // TBD
-    // this.router.post('/users/:userId/password', this.sendEmailResetPwd);
-    // this.router.post('/reset-password', this.resetPassword);
+    this.router.post(
+      '/users/:userId/password',
+      passport.authenticate('jwt', { session: false }),
+      this.resetPassword
+    );
   }
 
   login = async (req, res) => {
@@ -114,6 +117,76 @@ class AuthController {
       apiResponse.created(createUserResponse);
     } catch (error) {
       apiResponse.internalServerError(error);
+    }
+    return res.response(apiResponse);
+  };
+
+  resetPassword = async (req, res) => {
+    const apiResponse = new ApiResponse();
+    try {
+      const { userId } = req.params;
+      const { password } = req.body;
+
+      // TODO:
+      // validate password criteria
+
+      // Step get user
+      const getUserResponse = await this.usersService.getById(userId);
+      if (getUserResponse.fields.length) {
+        apiResponse.badRequest('Invalid user id provided');
+        return res.response(apiResponse);
+      }
+      if (!getUserResponse.result) {
+        apiResponse.notFound('User is not found :o');
+        return res.response(apiResponse);
+      }
+      const { username } = getUserResponse.result;
+
+      // Step get login account
+      const getUsernameResponse = await this.loginsService.findByUsername(
+        username
+      );
+      if (getUsernameResponse.fields.length) {
+        apiResponse.unprocessableEntity(
+          'Cannot reset the password, try again later'
+        );
+        return res.response(apiResponse);
+      }
+      if (!getUsernameResponse.result) {
+        apiResponse.unprocessableEntity(
+          'Cannot reset the password, try again later'
+        );
+        return res.response(apiResponse);
+      }
+      const loginId = getUsernameResponse.result.id;
+
+      // Step generate new pwd
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      // TODO:
+      // Step validate it is a self pwd reset
+      // auth requester user === params.userId
+
+      // Step update account password
+      const updatePwd = { passwordHash };
+      const updatePwdResponse = await this.loginsService.update(
+        loginId,
+        updatePwd
+      );
+      if (updatePwdResponse.fields.length) {
+        apiResponse.badRequest('Invalid password bro');
+        return res.response(apiResponse);
+      }
+      if (!updatePwdResponse.result) {
+        apiResponse.unprocessableEntity(
+          'Cannot update the password, try again later'
+        );
+        return res.response(apiResponse);
+      }
+      apiResponse.ok('Password has been reset successfully');
+    } catch (error) {
+      apiResponse.internalServerError(error.message);
     }
     return res.response(apiResponse);
   };
