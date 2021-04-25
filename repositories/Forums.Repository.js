c76566f1ae29659e8d0dd06fd840b568945fc9f7
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-25 13:59:59 -0700

    handle audience filter on forums repo

--- repositories/Forums.Repository.js
+++ repositories/Forums.Repository.js
@@ -23,6 +23,10 @@ class ForumsRepository {
       filter.$where = `this.participants.length >= ${from} && this.participants.length <= ${to}`;
     }
 
+    if (filters.audience) {
+      filter.isPrivate = filters.audience === 'public' ? false : true;
+    }
+
     if (filters.page) {
       options.page = filters.page;
     }
