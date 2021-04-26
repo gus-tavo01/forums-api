dd423ca08aa190b68af47581013c8787d520ebf1
Gustavo loera <gustavoa.loera01@gmail.com>
2021-04-25 23:21:32 -0700

    create topics mapper

---
+++ utilities/mappers/topicsList.js
@@ -0,0 +1,13 @@
+module.exports = (topics) => {
+  const result = { ...topics };
+  result.docs = topics.docs.map((topic) => ({
+    id: topic._id,
+    name: topic.name,
+    content: topic.content,
+    createDate: topic.createDate,
+    updateDate: topic.updateDate,
+    forumId: topic.forumId,
+    comments: topic.commentsLength,
+  }));
+  return result;
+};
