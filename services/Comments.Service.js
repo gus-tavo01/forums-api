76b61f976430a8fe879591234298b7839ab1d03a
Gustavo loera <gustavoa.loera01@gmail.com>
2021-05-12 20:13:20 -0700

    add create method on service

--- services/Comments.Service.js
+++ services/Comments.Service.js
@@ -13,6 +13,15 @@ class CommentsService {
       console.log(error);
     }
   };
+
+  create = async (message) => {
+    try {
+      const result = await this.commentsRepo.add(message);
+      return { payload: result, fields: [] };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 module.exports = CommentsService;
