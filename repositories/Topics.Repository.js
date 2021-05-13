0ea52aaadd7913dd15c75fdd03ff458add047657
Gustavo loera <gustavoa.loera01@gmail.com>
2021-05-12 20:12:27 -0700

    remove topics projection

--- repositories/Topics.Repository.js
+++ repositories/Topics.Repository.js
@@ -12,15 +12,6 @@ class TopicsRepository {
       page: 1,
       lean: true,
       leanWithId: true,
-      projection: {
-        id: 1,
-        name: 1,
-        content: 1,
-        createDate: 1,
-        updateDate: 1,
-        forumId: 1,
-        commentsLength: { $size: '$comments' },
-      },
     };
 
     if (filters.name) {
