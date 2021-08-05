bf1f60bf4a89f39cab0eb732c2e0ea8af5d477ed
Gustavo loera <gustavoa.loera01@gmail.com>
2021-08-04 23:53:33 -0700

    add participant(s) mappers

---
+++ utilities/mappers/participant.js
@@ -0,0 +1,9 @@
+module.exports = (data) => ({
+  id: data.id,
+  username: data.username,
+  avatar: data.avatar,
+  forumId: data.forumId,
+  userId: data.userId,
+  lastActivity: data.lastActivity,
+  role: data.role,
+});
