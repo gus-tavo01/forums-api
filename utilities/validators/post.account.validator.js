002be5ef149074913e55a0030ad027259c1a3d85
Gustavo loera <gustavoa.loera01@gmail.com>
2021-08-07 13:07:36 -0700

    add post account model validator

---
+++ utilities/validators/post.account.validator.js
@@ -0,0 +1,8 @@
+const Validations = require('../../common/constants/validations');
+
+module.exports = () => ({
+  username: [Validations.isEmpty()],
+  password: [Validations.isEmpty(), Validations.isLength({ min: 3 })],
+  email: [Validations.isEmpty(), Validations.isEmail()],
+  dateOfBirth: [Validations.isEmpty(), Validations.isDate()],
+});
