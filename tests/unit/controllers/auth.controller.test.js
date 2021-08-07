e86845150c2dcff770457b9c1cbba87803ae9444
Gustavo loera <gustavoa.loera01@gmail.com>
2021-08-07 14:41:56 -0700

    add login very basic unit test scenarios

--- tests/unit/controllers/auth.controller.test.js
+++ tests/unit/controllers/auth.controller.test.js
@@ -19,15 +19,12 @@ const authController = new AuthController();
 
 afterEach(() => {
   clearMockRes();
+  if (AccountsRepository.prototype.findByUsername) {
+    AccountsRepository.prototype.findByUsername.mockReset();
+  }
 });
 
 describe('Auth controller login', () => {
-  let authController = new AuthController();
-
-  afterEach(() => {
-    LoginsService.prototype.findByUsername.mockReset();
-  });
-
   test('When credentials are valid, expect a successful 200 response', async () => {
     // Arrange
     const username = 'rs.joemon001';
@@ -38,35 +35,82 @@ describe('Auth controller login', () => {
         password,
       },
     });
-    const mockAccount = {
-      fields: [],
-      result: {
-        username,
-        passwordHash:
-          '$2b$10$XDnIFdbz/R4qdLkhrAlco.LPJE2IlqdYOhKrprvGluBdS9vH.Ujzu',
-      },
-    };
-    LoginsService.prototype.findByUsername = jest.fn(async () => mockAccount);
 
-    const expectedStatusCode = 200;
+    // mocks
+    AccountsRepository.prototype.findByUsername = jest.fn(async () => ({
+      username,
+      passwordHash:
+        '$2b$10$XDnIFdbz/R4qdLkhrAlco.LPJE2IlqdYOhKrprvGluBdS9vH.Ujzu',
+    }));
 
     // Act
     const response = await authController.login(req, res);
 
     // Assert
     expect(response).toMatchObject({
-      payload: expect.anything(),
-      statusCode: expectedStatusCode,
+      message: 'Ok',
+      statusCode: 200,
+      payload: {
+        expiresIn: '120 Minutes',
+      },
+      errorMessage: null,
+      fields: [],
+    });
+  });
+
+  test('When username is invalid, expect a 400 response', async () => {
+    // Arrange
+    const username = undefined;
+    const password = 'password!';
+    const req = getMockReq({
+      body: {
+        username,
+        password,
+      },
+    });
+
+    // Act
+    const response = await authController.login(req, res);
+
+    // Assert
+    expect(response).toMatchObject({
+      message: 'Bad_Request',
+      statusCode: 400,
+      payload: null,
+      errorMessage: 'Validation errors',
+      fields: [`Field 'username' is not a string, got '${username}'`],
+    });
+  });
+
+  test('When password is invalid, expect a 400 response', async () => {
+    // Arrange
+    const username = 'shaq.01';
+    const password = null;
+    const req = getMockReq({
+      body: {
+        username,
+        password,
+      },
+    });
+
+    // Act
+    const response = await authController.login(req, res);
+
+    // Assert
+    expect(response).toMatchObject({
+      message: 'Bad_Request',
+      statusCode: 400,
+      payload: null,
+      errorMessage: 'Validation errors',
+      fields: [`Field 'password' is not a string, got '${password}'`],
     });
   });
 
   // test('When username does not exist, expect a 401 response', async () => {});
 
-  // test('When password is incorrect, expect a 401 response', async () => {});
+  // test('When password does not match, expect a 401 response', async () => {});
 
-  // test('When username is invalid, expect a 400 response', async () => {});
-
-  // test('When an unhandled error, expect a 500 response', async () => {});
+  // test('When an unhandled error occurs, expect a 500 response', async () => {});
 });
 
 describe('Auth controller register', () => {
