f9ccd88fe819bb1d8f2b37a3f48364d2d12667f8
Gustavo loera <gustavoa.loera01@gmail.com>
2021-08-08 10:46:07 -0700

    add happy path unit test scenario

---
+++ tests/unit/controllers/participants.controller.test.js
@@ -0,0 +1,84 @@
+require('dotenv').config();
+const { getMockReq } = require('@jest-mock/express');
+const { res, clearMockRes } = require('../../helpers/mockResponse')();
+const ParticipantsController = require('../../../controllers/Participants.Controller');
+// repositories
+const ForumsRepository = require('../../../repositories/Forums.Repository');
+const AccountsRepository = require('../../../repositories/Accounts.Repository');
+const ParticipantsRepository = require('../../../repositories/Participants.Repository');
+// mocks
+jest.mock('../../../repositories/Forums.Repository');
+jest.mock('../../../repositories/Accounts.Repository');
+jest.mock('../../../repositories/Participants.Repository');
+
+const participantsController = new ParticipantsController();
+
+afterEach(() => {
+  clearMockRes();
+});
+
+describe('Participants Controller POST', () => {
+  test('When participant data is valid, expect a successful response', async () => {
+    // Arrange
+    const participantData = { username: 'ticky.perez', role: 'participant' };
+    const requestor = { username: 'testerDeveloper001', role: 'Operator' };
+    const forumId = '610ee6890a25e341708f1706';
+    const participantUserId = '610ee6890a25e341708f1703';
+    const req = getMockReq({
+      params: { forumId },
+      body: participantData,
+      user: requestor,
+    });
+
+    // mocks
+    AccountsRepository.prototype.findByUsername = jest.fn(async () => ({
+      id: participantUserId,
+      isActive: true,
+    }));
+    ForumsRepository.prototype.findById = jest.fn(async () => ({
+      id: forumId,
+      isActive: true,
+      participants: 10,
+    }));
+    const mockGetParticipant = {
+      ...participantData,
+      userId: participantUserId,
+      forumId,
+    };
+    ParticipantsRepository.prototype.add = jest.fn(
+      async () => mockGetParticipant
+    );
+    ForumsRepository.prototype.modify = jest.fn(async () => ({
+      id: forumId,
+    }));
+
+    // Act
+    const apiResponse = await participantsController.post(req, res);
+
+    // Assert
+    expect(apiResponse).toMatchObject({
+      payload: mockGetParticipant,
+      statusCode: 200,
+      errorMessage: null,
+      fields: [],
+    });
+  });
+
+  // test('When request body is invalid, expect a 400 response with validation errors', async () => {});
+
+  // test('When forumId is invalid, expect a 400 response with validation errors', async () => {});
+
+  // test('When requestor authenticated, expect a 403 response', async () => {});
+
+  // test('When requestor role is unauthorized, expect a 403 response', async () => {});
+
+  // test('When source account is not found, expect a 422 response', async () => {});
+
+  // test('When source account is inactive, expect a 422 response', async () => {});
+
+  // test('When target forum is not found, expect a 422 response', async () => {});
+
+  // test('When target forum is inactive, expect a 422 response', async () => {});
+
+  // test('When increase forum participants count fails, expect to rollback the participant', async () => {});
+});
