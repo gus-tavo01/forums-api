4e18168e9bffe9cfec3a45fd9227593910457e1f
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-24 21:31:52 -0700

    fix boolean validator

--- utilities/validations/isBool.js
+++ utilities/validations/isBool.js
@@ -1,4 +1,10 @@
+const validator = require('validator');
 const executeValidator = require('../../common/processors/errorManager/executeValidator');
 
 module.exports = (value, key) =>
-  executeValidator(value, key, 'Boolean', () => typeof value !== 'boolean');
+  executeValidator(
+    value,
+    key,
+    'Boolean',
+    () => !validator.isBoolean(value.toString())
+  );
