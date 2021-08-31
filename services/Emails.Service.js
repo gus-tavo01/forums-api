f5710860934c7446aa09f8b110c111fe49ad7f71
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-08-31 16:40:23 -0700

    handle development mock service instance

--- services/Emails.Service.js
+++ services/Emails.Service.js
@@ -2,8 +2,17 @@ const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 
 class EmailsService {
-  send = async (email) => {
-    try {
+  constructor() {
+    if (process.env.DEVELOPMENT) {
+      this.transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PWD,
+        },
+      });
+    } else {
       const auth = {
         auth: {
           api_key: process.env.MG_API_KEY,
@@ -11,9 +20,13 @@ class EmailsService {
         },
       };
 
-      const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+      this.transporter = nodemailer.createTransport(mg(auth));
+    }
+  }
 
-      return nodemailerMailgun.sendMail(email);
+  send = async (email) => {
+    try {
+      return this.transporter.sendMail(email);
     } catch (err) {
       return { message: err.message };
     }
