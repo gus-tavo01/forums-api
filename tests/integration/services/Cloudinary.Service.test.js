434f8f4c9bd4caa065b4c851214a3c482c129b01
Gustavo Loera <gustavoa.loera01@gmail.com>
2021-09-01 21:40:23 -0700

    update integration test

--- tests/integration/services/Cloudinary.Service.test.js
+++ tests/integration/services/Cloudinary.Service.test.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const CloudinaryService = require('../../../services/Cloudinary.Service');
 const { imageUrl } = require('../../helpers/mockImageUrl');
 
+const ImageFolders = require('../../../common/constants/imagefolders');
+
 const cloudinaryService = new CloudinaryService();
 
 describe('Cloudinary Service uploadImage', () => {
   test('When image data is a valid image, expect to be uploaded on cloud', async () => {
     // Arrange
     const image = imageUrl;
-    const preset = 'setAvatar';
+    const preset = ImageFolders.avatars;
 
     // Act
     const uploadResponse = await cloudinaryService.uploadImage(image, preset);
