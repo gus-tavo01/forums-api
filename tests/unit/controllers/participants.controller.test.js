e545f28be2893d63466d9c8d85cba5919669ebe7
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-15 18:53:49 -0800

    add test scenario where requestor is not the forum operator

--- tests/unit/controllers/participants.controller.test.js
+++ tests/unit/controllers/participants.controller.test.js
@@ -1959,7 +1959,66 @@ describe('Participants Controller PATCH', () => {
     });
   });
 
-  // test('When request role is operator and requestor is not the current operator, expect a forbidden response', async () => {});
+  test('When request role is operator and requestor is not the current operator, expect a forbidden response', async () => {
+    // Arrange
+    const requestorUser = {
+      username: 'Jhon.Doe',
+      userId: '625aa6890a25e341708c1899',
+      participantId: '610ee6890a25e856708f1298',
+      role: Roles.Administrator,
+    };
+    const targetForum = {
+      id: '610ee6890a25e341708f1702',
+    };
+    const targetUser = {
+      username: 'TargetMe',
+      participantId: '610ee6890a25e541908e1852',
+      role: Roles.Participant,
+    };
+
+    const body = { role: Roles.Operator };
+    const req = getMockReq({
+      params: {
+        forumId: targetForum.id,
+        participantId: targetUser.participantId,
+      },
+      user: {
+        username: requestorUser.username,
+      },
+      body,
+    });
+
+    // mocks
+    ParticipantsRepository.prototype.findById = jest.fn();
+    ParticipantsRepository.prototype.findById.mockResolvedValueOnce({
+      id: targetUser.participantId,
+      username: targetUser.username,
+      role: targetUser.role,
+    });
+    ForumsRepository.prototype.findById = jest.fn(async () => ({
+      id: targetForum.id,
+    }));
+    UsersRepository.prototype.findByUsername = jest.fn(async () => ({
+      id: requestorUser.userId,
+    }));
+    ParticipantsRepository.prototype.findByUserAndForum = jest.fn(async () => ({
+      id: requestorUser.participantId,
+      username: requestorUser.username,
+      role: requestorUser.role,
+    }));
+
+    // Act
+    const response = await participantsController.patch(req, res);
+
+    // Assert
+    expect(response).toMatchObject({
+      statusCode: 403,
+      message: 'Forbidden',
+      fields: [],
+      payload: null,
+      errorMessage: 'Missing role permissions to perform this action',
+    });
+  });
 
   // test('When request role is operator and requestor is the current operator, expect operator to be updated', async () => {});
   // #endregion business logic
