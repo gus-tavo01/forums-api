497d28b573a44ebb6b73244c509872a5f2afecaa
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-22 09:26:01 -0800

    update post forum model validator

--- utilities/validators/post.forum.validator.js
+++ utilities/validators/post.forum.validator.js
@@ -1,9 +1,14 @@
-const Validations = require('../../common/constants/validations');
+const Validations = require('js-validation-tool/core/validations');
 
-module.exports = () => ({
-  // TODO
-  // image?
-  topic: [Validations.isEmpty()],
-  description: [Validations.isEmpty()],
-  isPrivate: [Validations.isBool()],
+module.exports = (entity) => ({
+  image: [
+    Validations.common.isOptional('image', entity.image),
+    Validations.string.isString('image', entity.image),
+    Validations.string.isNotEmpty('image', entity.image),
+  ],
+  topic: [Validations.string.isNotEmpty('topic', entity.topic)],
+  description: [
+    Validations.string.isNotEmpty('description', entity.description),
+  ],
+  isPrivate: [Validations.boolean.isBool('isPrivate', entity.isPrivate)],
 });
